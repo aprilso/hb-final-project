fc645014f01503b8bdf6201ab477f5fdb6231144
April Soetarman <aprilsoetarman@gmail.com>
2021-07-19 23:54:41 -0700

    setting up App.jsx

---
+++ static/js/App.jsx
@@ -0,0 +1,26 @@
+function App() {
+
+  function viewSchedule() {
+    //TO-DO
+  }
+
+  function addTaskToSchedule() {
+    // TO-DO
+  }
+
+  function markTaskComplete() {
+    //TO-DO
+  }
+
+  function addNoteToCalendar() {
+    //TO-DO
+  }
+
+  function addEventoccurrence() {
+    //TO-DO 
+  }
+
+
+}
+// ----- All of the above will render on the html page with the tag root -----
+ReactDOM.render(<App />, document.querySelector("#root"));
