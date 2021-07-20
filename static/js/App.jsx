efab393ab41e86bd2feed33a41549304b30e070a
April Soetarman <aprilsoetarman@gmail.com>
2021-07-20 00:16:10 -0700

    created React addTasktoSchedule function

--- static/js/App.jsx
+++ static/js/App.jsx
@@ -4,8 +4,63 @@ function App() {
     //TO-DO
   }
 
-  function addTaskToSchedule() {
-    // TO-DO
+  function viewTask(props) {
+    return (
+      <div className="task">
+        <p>Task: {props.task} </p> 
+        <p>Instructions: {props.instructions} </p>
+        <p>Frequency: {props.frequency}</p>
+        <p>Created by (opional): {props.user_id}</p> 
+      </div>
+    );
+  }
+
+  function addTaskToSchedule(props) {
+    const[task, setTask] = React.useState("");
+    const[frequency, setFrequency] = React.useState("");
+    const[instructions, setInstructions] = React.useState("");
+    function addNewTask() {
+      fetch("add-task", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json", //example - 
+        },
+        body: JSON.stringify({ task, frequency }), //example - 
+        }).then((response)) => { //JSON EXAMPLE BELOW
+          response.json().then((jsonResponse) => {
+            const {
+              taskAdded: { taskId, task, frequency },
+            } = jsonResponse; 
+            props.addCard(cardId, name, skill);
+        });
+      });
+    }
+    return (
+      <React.Fragment>
+        <h2>Add New Task</h2>
+        <label htmlFor="taskInput">Task Name</label>
+        <input
+          value={task}
+          onChange={(event) => setTask(event.target.value)}
+          id="taskInput"
+        ></input>
+        <label htmlFor="frequencyInput">Frequency (make this an enum dropdown)</label>
+        <input
+          value= {frequency}
+          onChange={(event) =>setFrequency(event.target.value)}
+          id="frequencyInput"
+        ></input>
+        <label htmlFor="taskInstructions">Instructions</label>
+        <input
+          value={instructions}
+          onChange= {(event) => setInstructions(event.target.value)}
+          id="taskInstructions"
+        ></input>
+        <button onClick = {addNewTask}>
+          Add
+        </button>
+      </React.Fragment>
+    );
   }
 
   function markTaskComplete() {
